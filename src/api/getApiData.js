1a912cb4c5c2dc1fc2439b1376961a95251d8d9d
matiasiaquinta <matias.iaquinta@gmail.com>
2024-09-23 16:06:00 -0300

    deploy settings

--- src/api/getApiData.js
+++ src/api/getApiData.js
@@ -4,8 +4,8 @@ export const getApiData = async (type) => {
       ? `${process.env.NEXT_PUBLIC_API_URL}/carousel/tag`
       : `${process.env.NEXT_PUBLIC_API_URL}/carousel/voucher`;
 
-  console.log(url);
   const api = await fetch(url);
   const response = await api.json();
+
   return { response };
 };
